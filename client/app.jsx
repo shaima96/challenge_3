9d637c80577a67d62f67c9d8901ac7c129e1948e
shaima96 <141062@ppu.edu.ps>
2020-11-04 13:59:37 +0200

    part 2

--- client/app.jsx
+++ client/app.jsx
@@ -1,13 +1,19 @@
+class App extends React.Component {
+    constructor() {
+        super()
+    }
+    render() {
+        return (
+            <div>
+               <h1>sssssssssssssssss</h1> 
+        
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"))
 
 
-var App=()=>(
-    <div>
-        Hello
-    </div>
-);
-
-ReactDOM.render(<App />, document.getElementById("app"));
-
-
-
+// export default App;
 
--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 
 
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-app.set('views', `${__dirname}/views`);
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/./public'))
+app.use(express.json())
 
-
-app.get('/', (req, res) => {
-    res.send(' show html page ');
+ 
+let port = 4000;
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + '/./client/app.js')
+})
+app.listen(port, () => {
+    console.log(`listening to port 4000`);
 });
-
-
